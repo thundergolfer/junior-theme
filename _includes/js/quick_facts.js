97cf1b28202d49f428c3348ca196041da0b10613
Jonathon Belotti <jbelotti@zendesk.com>
2017-09-29 21:38:57 +1000

    code now hackily handles projects.html page

--- _includes/js/quick_facts.js
+++ _includes/js/quick_facts.js
@@ -1,7 +1,13 @@
 var post_content = document.getElementsByClassName("post-content")[0];
-var facts_p = post_content.getElementsByTagName("p")[0];
-facts_p.setAttribute("style", "text-align: center; padding-bottom: 2%;");
-var facts_text = facts_p.innerHTML;
+
+if (post_content) {
+  var facts_p = post_content.getElementsByTagName("p")[0];
+  facts_p.setAttribute("style", "text-align: center; padding-bottom: 2%;");
+  var facts_text = facts_p.innerHTML;
+} else {  // not on about.md, must be on projects.html
+  facts_p = document.getElementsByClassName("measure")[0].getElementsByTagName("p")[0];
+  var facts_text = facts_p.innerHTML;
+}
 
 // we won't remove the facts <p> tag we will just remove what's inside it
 var new_html = "";
@@ -9,7 +15,7 @@ var new_html = "";
 facts = facts_text.split("@@ ");
 
 facts.forEach(function(fact) {
-  if (fact) {
+  if (fact.replace(/\s/g, '').length) {
     var regex = /(.*)\[(.*)\]\((.*)\)(.*)/; // checks for markdown hyperlink
     var matches = regex.exec(fact);
 
